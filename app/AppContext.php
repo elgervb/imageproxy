eefe889daec07c0f287b7826aeb05aa1fdccf690
Elger van Boxtel <elgervb@gmail.com>
2015-08-28 11:27:48 +0200

    parse filter options

--- app/AppContext.php
+++ app/AppContext.php
@@ -42,19 +42,46 @@ class AppContext implements IAppContext
         
         
         /**
-         * Returns the requested image 
+         * Returns the requested image
+         * 
+         * Options: rate:5|opacity:10
          */
-        $router->add('^/img/(.*)/(.*)$', function($filter, $image){
+        $router->add('^/img/([a-z]+)/?(.*)?/(.*\.[a-z]+)', function($filter, $optionsString, $image){
         	$image = new \SplFileInfo(__DIR__ . '/img/' . $image);
         	$controller = new \controllers\FilterController();
         	$method = str_replace(['-','_'], [''], $filter);
-        	$options = Context::get()->http()->getRequest()->getGet();
+        	$options = $this->parseOptions($optionsString);
         	
         	if (method_exists($controller, $method)) {
         	    return $controller->$method($image, $options);
         	}
         	throw new \Exception("Method " . $method . " not found on " . get_class($controller));
         });
+    }
+    
+    /**
+     * Parse the options string: rate:5;opacity:10
+     * 
+     * @param string $optionsString
+     * 
+     * @return array
+     */
+    private function parseOptions($optionsString = null){
+        if (!$optionsString){
+            return [];
+        }
+        $options = explode(";", $optionsString);
+        if (count($options) === 0){
+            $options[] = $optionsString;
+        }
+        
+        $result = array();
+        foreach ($options as $value){
+            $o = explode(':', $value);
+            $result[$o[0] ] = $o[1];
+        }
+        
+        return $result;
     }
 
     /**
