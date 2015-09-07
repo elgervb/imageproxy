b788ce8a15c7b3c05248ec71add175eb31b651a4
Elger van Boxtel <elgervb@gmail.com>
2015-09-07 14:23:19 +0200

    add thumbnail support
    #3
    max pixel thumbnail support

--- app/controllers/ThumbnailController.php
+++ app/controllers/ThumbnailController.php
@@ -20,4 +20,13 @@ class ThumbnailController
         
         return ImageBuilder::create($image)->thumbCentered($width, $width);
     }
+    
+    public function max(SplFileInfo $image, array $options = null){
+    
+        // TODO cast to int workaround for https://github.com/elgervb/imagemanipulation/issues/25
+        $width = (int) Options::option($options, 'width', '250');
+        $height = (int) Options::option($options, 'height', '250');
+    
+        return ImageBuilder::create($image)->thumbMax($width, $height);
+    }
 }
